a742bfda84229ebb126321efd7980fddacb62a25
Sanket <sanketwakhare@gmail.com>
2021-11-28 23:41:45 +0530

    add sales list data in page

--- angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+
+  salesPersons: SalesPerson[] = [
+    new SalesPerson("John", "Doe", "johndoe@gmail.com", 342320),
+    new SalesPerson("David", "Smith", "davsmith@gmail.com", 435400),
+    new SalesPerson("Michael", "Lorenz", "mak@gmail.com", 12000),
+    new SalesPerson("Alex", "Miller", "alex@gmail.com", 85000),
+  ];
 
   constructor() { }
 
